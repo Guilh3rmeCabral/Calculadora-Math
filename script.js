5d98b2383c42091b3e0e50be4e4ec29629917e6a
Guilherme Cabral <guicabralprowork@gmail.com>
2022-08-24 16:09:29 -0300

    Refatorando codigo js

--- script.js
+++ script.js
@@ -1,41 +1,28 @@
+const display = document.getElementById('resultado');
+const classList = document.querySelector("body").classList;
+
 function insert(num) {
-    var numero = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = numero +  num;
+    display.innerHTML += num;
 }
 
 function clean() {
-    document.getElementById('resultado').innerHTML = "";
+    display.innerHTML = "";
 }
 
 function back() {
-    var resultado = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+    display.innerHTML = display.innerHTML.substring(0, display.innerHTML.length -1);
 }
 
 function calcular() {
-    var resultado = document.getElementById('resultado').innerHTML;
+    const resultado = display.innerHTML;
     if(resultado) {
-        document.getElementById('resultado').innerHTML = eval(resultado);
+        display.innerHTML = eval(resultado);
     }
 }
 
 
 // Funções e eventos para mudanção de tema
-let tela = document.querySelector('body');
-var btnLight = document.querySelector('#btlight');
-var btnDark = document.querySelector('#btdark');
-
-btnDark.addEventListener('click', modoDark);
-btnLight.addEventListener('click', modoLight);
-
-
-
-function modoDark(){
-    tela.classList.add("dark");
-    tela.classList.remove("light");
-}
-
-function modoLight(){
-    tela.classList.remove("dark");
-    tela.classList.add("light");   
+function changeTheme(theme) {
+    theme == 'dark' ? classList.add("dark") : classList.add("light");
+    theme == 'dark' ? classList.remove("light") : classList.remove("dark");
 }
